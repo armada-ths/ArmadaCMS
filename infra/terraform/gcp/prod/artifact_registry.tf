bbb0cf83414f58e091aca190949f8408c2d05588
Einar Harri <77612217+einhar1@users.noreply.github.com>
2026-04-07 10:52:35 +0200

    fix: small adjustments

--- infra/terraform/gcp/prod/artifact_registry.tf
+++ infra/terraform/gcp/prod/artifact_registry.tf
@@ -2,7 +2,7 @@ resource "google_artifact_registry_repository" "docker" {
   project       = var.project_id
   location      = var.region
   repository_id = var.artifact_registry_repository_id
-  description   = "Docker images for ArmadaCMS Cloud Run deployments"
+  description   = "Cloud Run Source Deployments"
   format        = "DOCKER"
   labels        = local.labels
 
