61eb1029971bc5e72dc16f29c19aa7198dd7dc36
Einar Harri <77612217+einhar1@users.noreply.github.com>
2026-04-12 11:07:50 +0200

    fix: import exsting domain mapping

--- infra/terraform/gcp/staging/domain_mapping.tf
+++ infra/terraform/gcp/staging/domain_mapping.tf
@@ -12,6 +12,11 @@
 # PREREQUISITE: The domain staging.cms.armada.nu must be verified in Search
 # Console or via the Google Domains verification flow for the GCP project.
 
+import {
+  id = "locations/europe-north1/namespaces/just-sunrise-491718-m9/domainmappings/staging.cms.armada.nu"
+  to = google_cloud_run_domain_mapping.staging[0]
+}
+
 resource "google_cloud_run_domain_mapping" "staging" {
   count = var.enable_domain_mapping && var.deploy_cloud_run_service ? 1 : 0
 
