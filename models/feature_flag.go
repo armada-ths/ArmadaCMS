b4c15666270076f562ee39aa8c4f2f115a1b0736
Einar Harri <77612217+einhar1@users.noreply.github.com>
2026-04-16 22:50:41 +0200

    feat: add new feature flags

--- models/feature_flag.go
+++ models/feature_flag.go
@@ -16,7 +16,7 @@ var DefaultFeatureFlags = []FeatureFlag{
 	{
 		Key:         "MAP_PAGE",
 		Description: "Show the fair map page",
-		Enabled:     false,
+		Enabled:     true,
 	},
 	{
 		Key:         "AT_FAIR_PAGE",
@@ -26,16 +26,51 @@ var DefaultFeatureFlags = []FeatureFlag{
 	{
 		Key:         "EXHIBITOR_PACKAGES",
 		Description: "Show the exhibitor packages page",
-		Enabled:     false,
+		Enabled:     true,
 	},
 	{
 		Key:         "EXHIBITOR_EVENTS",
 		Description: "Show the exhibitor events page",
-		Enabled:     false,
+		Enabled:     true,
 	},
 	{
 		Key:         "EXHIBITOR_PAGE",
 		Description: "Show the student exhibitors/companies page",
 		Enabled:     true,
+	},
+	{
+		Key:         "STUDENT_RECRUITMENT_PAGE",
+		Description: "Show the student recruitment page",
+		Enabled:     true,
+	},
+	{
+		Key:         "EXHIBITOR_MAIN_PAGE",
+		Description: "Show the exhibitor main/why armada page",
+		Enabled:     true,
+	},
+	{
+		Key:         "EXHIBITOR_TIMELINE_PAGE",
+		Description: "Show the exhibitor timeline page",
+		Enabled:     true,
+	},
+	{
+		Key:         "EXHIBITOR_SIGNUP_PAGE",
+		Description: "Show the exhibitor signup/registration page (only controls the topnav link, the page itself is controlled by the exhibitor timeline).",
+		Enabled:     true,
+	},
+	{
+		Key:         "ABOUT_PAGE",
+		Description: "Show the about armada page",
+		Enabled:     true,
+	},
+	{
+		Key:         "ABOUT_TEAM_PAGE",
+		Description: "Show the about team page",
+		Enabled:     true,
+	},
+	{
+		Key:         "ARMADA_BLOG_PAGE",
+		Description: "Show the armada blog page",
+		Enabled:     false,
 	},
 }
