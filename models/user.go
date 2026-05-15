a6a437c507acecfc1982d4f639e96723a5db55de
Einar Harri <77612217+einhar1@users.noreply.github.com>
2026-05-15 23:58:02 +0200

    fix: unique usernames

--- models/user.go
+++ models/user.go
@@ -4,7 +4,7 @@ import "time"
 
 type User struct {
 	ID        uint      `gorm:"primaryKey;autoIncrement;column:id;not null" json:"id"`
-	Username  string    `gorm:"column:username;not null" json:"username"`
+	Username  string    `gorm:"column:username;uniqueIndex;not null" json:"username"`
 	Password  string    `gorm:"column:password;not null" json:"-"`
 	Name      string    `gorm:"column:name;not null" json:"name"`
 	Avatar    string    `gorm:"column:avatar" json:"avatar"`
