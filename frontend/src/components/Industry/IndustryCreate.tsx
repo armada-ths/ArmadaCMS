4152361c58531d38a51445d5430f927806759a78
Einar Harri <77612217+einhar1@users.noreply.github.com>
2026-09-21 17:38:13 +0200

    test: trigger CD

--- frontend/src/components/Industry/IndustryCreate.tsx
+++ frontend/src/components/Industry/IndustryCreate.tsx
@@ -1,5 +1,7 @@
 import { Create, CreateProps, SimpleForm, TextInput } from "react-admin";
 
+// test comment to trigger CD
+
 export const IndustryCreate = (props: CreateProps) => (
   <Create {...props}>
     <SimpleForm>
